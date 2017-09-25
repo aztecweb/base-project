24e522be006119ef87b4f136215f188ff17a095f
Eduardo Pittol <edpittol@gmail.com>
2017-09-25 00:26:44 -0300

    Add integration with Deployer

---
+++ deploy.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Deploy script using deployer.org.
+ */
+namespace Deployer;
+
+require 'recipe/common.php';
+require 'recipe/npm.php';
+require 'extra/recipe/grunt.php';
+
+host('baseproject.aztecweb.net')
+	->stage('production')
+	->user('baseproject')
+	->set('deploy_path', '/home/baseproject')
+	->set('http_user', 'www-data');
+
+set('repository', 'git@greatcode.aztecweb.net:aztecwebteam/base-project.git');
+set('branch', 'master');
+
+set('shared_files', [
+	'.env'
+]);
+
+set('shared_dirs', [
+	'public/wp-content/uploads'
+]);
+
+set('writable_dirs', [
+	'public/wp-content/uploads'
+]);
+
+set('ssh_multiplexing', false);
+
+task('reload:php-fpm', function () {
+    run('sudo /usr/sbin/service php7.1-fpm reload');
+});
+
+task('deploy:install', function () {
+    run('bin/install');
+});
+
+task('deploy', [
+	'deploy:prepare',
+	'deploy:lock',
+	'deploy:release',
+	'deploy:update_code',
+	'deploy:shared',
+	'deploy:writable',
+	'deploy:vendors',
+	'deploy:install',
+	'deploy:clear_paths',
+	'deploy:symlink',
+	'deploy:unlock',
+	'reload:php-fpm',	
+	'cleanup',
+	'success'
+]);
